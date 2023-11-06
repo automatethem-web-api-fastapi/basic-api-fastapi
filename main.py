51bcf033395be849f8c030a41aed00584927af23
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-11-06 17:31:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-fast_api = FastAPI()
+app = FastAPI()
 
 from api.basic_api_fastapi import api as api_basic_pi_fastapi_api
-api_basic_pi_fastapi_api.register(fast_api)
+api_basic_pi_fastapi_api.register(app)
