535523d9f5671bac1a851cc3218162c42ebd0e46
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-11-06 17:34:02 +0900

    Update uvicorn_main_fast_api.sh

--- bin/uvicorn_main_fast_api.sh
+++ bin/uvicorn_main_fast_api.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ..
 rm nohup.out
-#nohup uvicorn main:fast_api --reload --host 0.0.0.0 --port 8000 & 
-nohup uvicorn main:fast_api --reload --host 0.0.0.0 & 
+#nohup uvicorn main:app --reload --host 0.0.0.0 --port 8000 & 
+nohup uvicorn main:app --reload --host 0.0.0.0 & 
