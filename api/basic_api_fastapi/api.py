7de6528076faed9d3eb3cef154bb205670fcb04b
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-06 12:09:43 +0900

    Update api.py

--- api/basic_api_fastapi/api.py
+++ api/basic_api_fastapi/api.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, Request
 from pydantic import BaseModel
-from .handler import EndpointHandler
 import json
 from typing import Any
 from typing import Union
@@ -14,11 +13,17 @@ class RequestData(BaseModel):
     inputs: Union[Any, Dict]
     parameters: Optional[Dict]
             
-async def api(request_data: RequestData):
+async def get(inputs):
+    print(inputs)
+    prediction = [{'label': 'pos', 'score': 0.8038843274116516}]
+    return prediction
+
+async def post(request_data: RequestData):
     inputs = request_data.json()
     inputs = json.loads(inputs)
     prediction = [{'label': 'pos', 'score': 0.8038843274116516}]
     return prediction
-
+    
 def register(app):
-    app.add_api_route(path="/api/basic-api-fastapi/api", endpoint=api, methods=["POST"])
+    app.add_api_route(path="/api/basic-api-fastapi/get", endpoint=api, methods=["GET"])
+    app.add_api_route(path="/api/basic-api-fastapi/post", endpoint=api, methods=["POST"])
