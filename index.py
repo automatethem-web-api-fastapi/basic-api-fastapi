2bfccc8a451629b829d76243cdc320910d3cded7
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2025-05-05 02:05:20 +0900

    Update index.py

--- index.py
+++ index.py
@@ -8,9 +8,75 @@ from typing import List
 from typing import Optional
 import os
 import asyncio
+from fastapi.middleware.cors import CORSMiddleware
 
 #app = FastAPI()
 app = FastAPI(docs_url="/api/fastapi/api/docs", openapi_url="/api/fastapi/api/openapi.json")
+
+allowed_origins = [
+    "https://ar.freeonlineutility.com",
+    "https://bn.freeonlineutility.com",
+    "https://zh.freeonlineutility.com",
+    "https://en.freeonlineutility.com",
+    "https://fr.freeonlineutility.com",
+    "https://de.freeonlineutility.com",
+    "https://hi.freeonlineutility.com",
+    "https://it.freeonlineutility.com",
+    "https://ja.freeonlineutility.com",
+    "https://ko.freeonlineutility.com",
+    "https://mr.freeonlineutility.com",
+    "https://fa.freeonlineutility.com",
+    "https://pt.freeonlineutility.com",
+    "https://pa.freeonlineutility.com",
+    "https://ru.freeonlineutility.com",
+    "https://es.freeonlineutility.com",
+    "https://ta.freeonlineutility.com",
+    "https://te.freeonlineutility.com",
+    "https://tr.freeonlineutility.com",
+    "https://ur.freeonlineutility.com",
+    "https://t.ar.freeonlineutility.com",
+    "https://t.bn.freeonlineutility.com",
+    "https://t.zh.freeonlineutility.com",
+    "https://t.en.freeonlineutility.com",
+    "https://t.fr.freeonlineutility.com",
+    "https://t.de.freeonlineutility.com",
+    "https://t.hi.freeonlineutility.com",
+    "https://t.it.freeonlineutility.com",
+    "https://t.ja.freeonlineutility.com",
+    "https://t.ko.freeonlineutility.com",
+    "https://t.mr.freeonlineutility.com",
+    "https://t.fa.freeonlineutility.com",
+    "https://t.pt.freeonlineutility.com",
+    "https://t.pa.freeonlineutility.com",
+    "https://t.ru.freeonlineutility.com",
+    "https://t.es.freeonlineutility.com",
+    "https://t.ta.freeonlineutility.com",
+    "https://t.te.freeonlineutility.com",
+    "https://t.tr.freeonlineutility.com",
+    "https://t.ur.freeonlineutility.com",
+    "https://freeonlineutility.com",
+    "https://www.freeonlineutility.com",
+]
+#allowed_origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # 'GET', 'POST', etc. or "*" for all
+    allow_headers=[
+        "Authorization",
+        "Content-Type",
+        "X-CSRF-Token",
+        "X-Requested-With",
+        "Accept",
+        "Accept-Version",
+        "Content-Length",
+        "Content-MD5",
+        "Date",
+        "X-Api-Version"
+    ],
+)
 
 #'''
 class RequestData(BaseModel):
